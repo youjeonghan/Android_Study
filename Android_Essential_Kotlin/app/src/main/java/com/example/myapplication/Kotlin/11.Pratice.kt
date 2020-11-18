9d284847a595784aa8f25740be716be622d532d4
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-11-19 02:10:00 +0900

    제어흐름 B 실습

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Kotlin/11.Pratice.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Kotlin/11.Pratice.kt
@@ -1,2 +1,55 @@
 package com.example.myapplication.Kotlin
 
+
+// 11. 제어흐름 실습
+
+fun main(args: Array<String>) {
+
+    val value: Int? = null
+
+    when (value) {
+        null -> println("value is null")
+        else -> println("value is not null")
+    }
+
+    val value2: Boolean? = null
+
+    // when 구문은 조건으로 갖는 값의 모든 경우에 대응 해주는것이 좋다
+    when (value2) {
+        true -> println("")
+        false -> println("")
+        null -> println("")
+    }
+
+    // 값을 리턴하는 when 구문의 경우 반듯이 값을 리턴해야 한다
+    val value3 = when (value2) {
+        true -> 1
+        false -> 3
+        null -> 4
+    }
+
+    // when의 다양한 조건식 (1)
+    val value4: Int = 10
+    when (value4) {
+        is Int -> {
+            println("value4 is int")
+        }
+        else -> {
+            println("value4 is not int")
+        }
+    }
+
+    // when의 다양한 조건식 (2)
+    val value5: Int = 87
+    when (value5) {
+        in 60..70 -> {
+            println("value is in 60-70")
+        }
+        in 70..80 -> {
+            println("value is in 70-80")
+        }
+        in 80..90 -> {
+            println("value is in 80-90")
+        }
+    }
+}
