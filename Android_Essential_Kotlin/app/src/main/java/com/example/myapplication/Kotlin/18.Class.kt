8ab2545f30ed33feb4374a69cfea5a79656756ae
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-12-28 13:22:09 +0900

    클래스 2

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Kotlin/18.Class.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Kotlin/18.Class.kt
@@ -18,7 +18,38 @@ package com.example.myapplication.Kotlin
 // 객체를 만드는 방법
 // - 설명서가 있어야 합니다
 
-fun main(array: Array<String>){
+fun main(array: Array<String>) {
 
+    // 클래스(설명서)를 통해서 실체를 만드는 방법
+    // -> 인스턴스화 -> 인스턴스(객체)
+    Car("v8 engine", "big")
+
+    val bigCar = Car("v8 engine", "big")
+
+    // 우리가 만든 클래스는 자료형이 된다
+    val bigCar1: Car = Car("v8 engine", "big")
+
+    val superCar: SuperCar = SuperCar("good engine", "big", "white")
+}
+
+// 클래스(설명서) 만드는 방법(1)
+class Car(var engine: String, var body: String) {
+
+}
+
+// 클래스(설명서) 만드는 방법(2)
+class SuperCar {
+    var engine: String
+    var body: String
+    var door: String
+
+    constructor(engine: String, body: String, door: String) {
+        println(engine)
+        println(body)
+        println(door)
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
 }
 
