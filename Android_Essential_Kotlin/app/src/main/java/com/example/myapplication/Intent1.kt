4235a9d92d9e6029b146f0cf2592f9b3f644b993
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-09 23:38:01 +0900

    Intent 실습 4

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Intent1.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Intent1.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,20 +19,24 @@ class Intent1 : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.changeActivity.setOnClickListener {
-            val intent = Intent(this@Intent1, Intent2::class.java)
+//            val intent = Intent(this@Intent1, Intent2::class.java)
 
 //            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다.
 //            intent.putExtra("number1", 1)
 //            intent.putExtra("number2", 2)
-//            startActivity(intent)
+//            startActivity(intent) 
 
-            val intent2 = Intent(this@Intent1, Intent2::class.java)
-            // Apply -> 사용하면 유지보수 및 실수가 준다
-            intent2.apply {
-                this.putExtra("number1", 1)
-                this.putExtra("number2", 2)
-            }
-            startActivityForResult(intent2, 200)
+//            val intent2 = Intent(this@Intent1, Intent2::class.java)
+//            // Apply -> 사용하면 유지보수 및 실수가 준다
+//            intent2.apply {
+//                this.putExtra("number1", 1)
+//                this.putExtra("number2", 2)
+//            }
+//            startActivityForResult(intent2, 200)
+
+            // 암시적 인텐트
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http:/m.naver.com"))
+            startActivity(intent)
         }
 
     }
