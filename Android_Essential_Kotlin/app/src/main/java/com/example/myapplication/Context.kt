029cef371f392f31d2c06dfa2a6352c2df8709d7
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-11 19:55:01 +0900

    Context
    - ApplicationContext, ActivityContext의 범위

---
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/Context.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Context : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_context)
+
+        val context: Context = this
+        val applicationContext = getApplicationContext()
+    }
+}
