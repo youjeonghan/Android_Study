2fbcf41389d6da2ee29379b0446dbf4248ee76f9
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-19 17:24:15 +0900

    Realm 실습
    - Realm에서 테이블에 접근하여 지우는것

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RealmActivity.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RealmActivity.kt
@@ -2,15 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.myapplication.databinding.ActivityRealmBinding
 import com.example.myapplication.databinding.ActivitySharedPreferenceBinding
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
 class RealmActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySharedPreferenceBinding
+    private lateinit var binding: ActivityRealmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySharedPreferenceBinding.inflate(layoutInflater)
+        binding = ActivityRealmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Realm.init(this@RealmActivity)
@@ -19,7 +21,38 @@ class RealmActivity : AppCompatActivity() {
             RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
         Realm.setDefaultConfiguration(config)
         // realm을 얻는 것
-        val ream = Realm.getDefaultInstance()
+        val realm = Realm.getDefaultInstance()
+
+        binding.buttonSave.setOnClickListener {
+            realm.executeTransaction {
+                // 트랜잭션은 작업을 묶는것 데이터 손상을 막기위해서 (저장하는 도중 어떤 데이터가 값이 변하는것)
+                // A테이블에서 데이터를 가져온다
+                // B테이블에서 데이터를 가져온다
+                // C테이블에서 데이터를 가져온다
+                // 조합을 한다
+                // D테이블에 저장을 한다
+                with(it.createObject(School::class.java)) {
+                    this.name = "어떤 대학교"
+                    this.location = "서울"
+                }
+            }
+        }
+        binding.buttonLoad.setOnClickListener {
+            realm.executeTransaction {
+                val data = it.where(School::class.java).findFirst()
+                Log.d("dataa", "data : " + data)
+            }
+        }
+        binding.buttonDelete.setOnClickListener {
+            realm.executeTransaction {
+                // 테이블을 전부 지우는 것
+                it.where(School::class.java).findAll().deleteAllFromRealm()
+                // 하나만 지우는 것
+//                it.where(School::class.java).findFirst().deleteFromRealm()
+
+            }
+        }
+
 
     }
 }
