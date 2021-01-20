86cfa281ee8a286d24bedd7a4308a0d499b41faa
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-20 21:40:49 +0900

    Async 실습 (진행하다 멈춤)
    - 현재는 공식적인 지원이 끊긴거를 알게 됬다.
    사용안할 예정

---
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/AsyncActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityAsyncBinding
+
+class AsyncActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAsyncBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAsyncBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
+
+class BackgroundAsyncTask(
+    val progressbar: ProgressBar,
+    val progressText: TextView
+) : AsyncTask<Int, Int, Int>() {
+    // parms -> doInBackground 에서 사용할 타입
+    // progress -> onProgressUpdate 에서 사용할 타입
+    // result -> onPostExcute 에서 사용할 타입
+}
