ee33cacee7d3dcf843aee549c3213d10d3fd6e81
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-22 18:09:09 +0900

    Network 실습 7
    - Retrofit으로 GET POST 이용

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitActivity.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitActivity.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityRetrofitBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class RetrofitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRetrofitBinding
@@ -27,13 +26,15 @@ class RetrofitActivity : AppCompatActivity() {
 
         val service = retrofit.create(RetrofitService::class.java)
 
-        service.getStudentsList().enqueue(object: Callback<ArrayList<PersonFromServer>>{
+        // GET 요청
+        // enqueue를 통해서 결과값을 Callback으로 넘겨받는다
+        service.getStudentsList().enqueue(object : Callback<ArrayList<PersonFromServer>> {
             // 통신 성공시 호출
             override fun onResponse(
                 call: Call<ArrayList<PersonFromServer>>,
                 response: Response<ArrayList<PersonFromServer>>
             ) {
-                if (response.isSuccessful){
+                if (response.isSuccessful) {
                     val personList = response.body()
                     Log.d("retrofitt", "res : " + personList?.get(0)?.age)
 
@@ -49,6 +50,45 @@ class RetrofitActivity : AppCompatActivity() {
             // 통신 실패시 호출
             override fun onFailure(call: Call<ArrayList<PersonFromServer>>, t: Throwable) {
                 Log.d("retrofitt", "ERROR")
+            }
+        })
+
+        // POST 요청 (1)
+//        val params = HashMap<String, Any>()
+//        params.put("name", "유정한")
+//        params.put("age", 20)
+//        params.put("intro", "안녕하세요")
+//        service.createStudents(params).enqueue(object : Callback<PersonFromServer> {
+//            override fun onResponse(
+//                call: Call<PersonFromServer>,
+//                response: Response<PersonFromServer>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val person = response.body()
+//                    Log.d("retrofitt", "name : " + person?.name)
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<PersonFromServer>, t: Throwable) {
+//                TODO("Not yet implemented")
+//            }
+//        })
+
+        // POST 요청 (2)
+        val person = PersonFromServer(name = "유정한", age = 26, intro = "안녕")
+        service.createStudentsEasy(person).enqueue(object : Callback<PersonFromServer> {
+            override fun onResponse(
+                call: Call<PersonFromServer>,
+                response: Response<PersonFromServer>
+            ) {
+                if (response.isSuccessful) {
+                    val person = response.body()
+                    Log.d("retrofitt", "name : " + person?.name)
+                }
+            }
+
+            override fun onFailure(call: Call<PersonFromServer>, t: Throwable) {
+                TODO("Not yet implemented")
             }
         })
     }
--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitService.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitService.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface RetrofitService {
 
     // "json/students/" 여기에서 response가 ArrayList<PersonFromServer>이다 란 말
     @GET("json/students/")
     fun getStudentsList(): Call<ArrayList<PersonFromServer>>
+
+    @POST("json/students/")
+    fun createStudents(
+        @Body params: HashMap<String, Any>
+    ): Call<PersonFromServer>
+
+    @POST("json/students/")
+    fun createStudentsEasy(
+        @Body person: PersonFromServer
+    ): Call<PersonFromServer>
 }
