aca8428e9be624330ca906b8513e46a6fb1d9838
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-22 17:24:33 +0900

    Network 실습 6
    - Retrofit 사용하기

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitActivity.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/RetrofitActivity.kt
@@ -2,8 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myapplication.databinding.ActivityRetrofitBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class RetrofitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRetrofitBinding
@@ -16,8 +22,34 @@ class RetrofitActivity : AppCompatActivity() {
         // http://mellowcode.org/test/code/
         val retrofit = Retrofit.Builder()
             .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val service = retrofit.create(RetrofitService::class.java)
+
+        service.getStudentsList().enqueue(object: Callback<ArrayList<PersonFromServer>>{
+            // 통신 성공시 호출
+            override fun onResponse(
+                call: Call<ArrayList<PersonFromServer>>,
+                response: Response<ArrayList<PersonFromServer>>
+            ) {
+                if (response.isSuccessful){
+                    val personList = response.body()
+                    Log.d("retrofitt", "res : " + personList?.get(0)?.age)
+
+                    val code = response.code()
+                    Log.d("retrofitt", "code : " + code)
+
+                    val error = response.errorBody()
+                    val header = response.headers()
+                    Log.d("retrofitt", "header : " + header)
+                }
+            }
+
+            // 통신 실패시 호출
+            override fun onFailure(call: Call<ArrayList<PersonFromServer>>, t: Throwable) {
+                Log.d("retrofitt", "ERROR")
+            }
+        })
     }
 }
