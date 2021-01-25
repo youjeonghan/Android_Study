0a681c59cc64a340c1c9b6b3db9d1d9b6d46db3c
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-26 02:04:45 +0900

    인스타그램 2
    - 어플 전반적인부분에서 레트로핏을 이용하게 만듬 (한 액티비티에서만 사용가능한게 아님)
    - stetho 이용

--- Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/EmailSignupActivity.kt
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/EmailSignupActivity.kt
@@ -2,10 +2,36 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.example.myapplication.databinding.ActivityEmailSignupBinding
 
 class EmailSignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEmailSignupBinding
+    lateinit var usernameView: EditText
+    lateinit var userPassword1View: EditText
+    lateinit var userPassword2View: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_email_signup)
+        binding = ActivityEmailSignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
     }
+
+    fun initView() {
+        usernameView = binding.usernameInputbox
+        userPassword1View = binding.password1Inputbox
+        userPassword2View = binding.password2Inputbox
+    }
+
+    fun getUserName(): String {
+        return usernameView.text.toString()
+    }
+    fun getUserPassword1():String{
+        return userPassword1View.text.toString()
+    }
+    fun getUserPassword2():String{
+        return userPassword2View.text.toString()
+    }
+
 }
---
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/MasterApplication.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication
+
+import android.app.Application
+import android.content.Context
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+// 액티비티보다 어플레이션 계층이 한단 위임 (oncreate 또한)
+// Manifest에 등록 해줘야함 android:name=".MasterApplication"
+class MasterApplication : Application() {
+
+    lateinit var service: RetrofitService
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+
+    fun createRetrofit() {
+        // 원래 나갈려던걸 붙잡고(인터셉트) 헤더를 붙여서 다시 내보내는 것(프로시드 = 다시 진행행)
+        val header = Interceptor {
+            val original = it.request()
+
+
+            if (checkIsLogin()) {
+                getUserToken()?.let { token ->
+                    val request = original.newBuilder()
+                        .header("Authorization", token)
+                        .build()
+                    it.proceed(request)
+                }
+            } else {
+                it.proceed(original)
+            }
+        }
+
+        // client 안에 위의 헤더가 들어있다
+        val client = OkHttpClient.Builder()
+            .addInterceptor(header)
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://melowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        service = retrofit.create(RetrofitService::class.java)
+    }
+
+    fun checkIsLogin(): Boolean {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sp.getString("login_sp", "null")
+        if (token != "null") return true
+        else return false
+    }
+
+    fun getUserToken(): String? {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sp.getString("login_sp", "null")
+        if (token == "null") return null
+        else return token
+    }
+}
