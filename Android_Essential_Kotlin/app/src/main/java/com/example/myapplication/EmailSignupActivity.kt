a25bc163d0f812ccd146aa8d4a6f80ec7eb06abb
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-01-25 13:54:32 +0900

    인스타그램 1
    - 인스타그램 기능 클론 코딩중

---
+++ Android_Essential_Kotlin/app/src/main/java/com/example/myapplication/EmailSignupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_signup)
+    }
+}
